da528dc4d73d87bd24ee45883f5df8d78da4056c
showayamada <yamasho082433@gmail.com>
2024-10-04 14:13:57 +0900

    wip:2F仕様の制約抽出

--- src/script.sh
+++ src/script.sh
@@ -1,6 +1,14 @@
 #!bin/bash
 include "automaton.hoa";
-convert -t nbw -o convertedAutomaton.hoa automaton.hoa;
-complement -m ramsey -o complemented.hoa convertedAutomaton.hoa;
+echo "automaton.hoa loaded";
+echo "simplifying automaton";
+$automaton = simplify -dse -ds -rse -rs -ru -rd automaton.hoa;
+echo "simplify success";
+convert -t nbw -o convertedAutomaton.hoa $automaton;
+echo "converted to nbw";
+$converted = simplify -dse -ds -rse -rs -ru -rd convertedAutomaton.hoa;
+complement -m ramsey -o complemented.hoa $converted;
+echo "complement success";
 $aut =  simplify -dse -ds -rse -rs -ru -rd complemented.hoa;
+echo "simplify success";
 save -c HOAF $aut simplified.hoa;
